d030dd7ef66b6dc180fe88eddda48a2979824514
Joerg Diekmann <joergd@pobox.com>
2008-11-25 14:32:00 +0200

    Added proper browser titles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,5 @@
 class PagesController < ApplicationController
 
-  before_filter :load_page, :only => :index
-  
-  # GET /pages
-  # GET /pages.xml
-  def index
-    @pages = paginate( Page, :order => 'title' )
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @pages }
-    end
-  end
-
   # GET /pages/1
   # GET /pages/1.xml
   def show
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,67 +2,6 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe PagesController do
   
-  describe "handling GET /pages" do
-
-    before(:each) do
-      @page = mock_model(Page)
-      Page.stub!(:find).and_return([@page])
-    end
-  
-    def do_get
-      get :index
-    end
-  
-    it "should be successful" do
-      do_get
-      response.should be_success
-    end
-
-    it "should render index template" do
-      do_get
-      response.should render_template('index')
-    end
-  
-    it "should find all pages" do
-      Page.should_receive(:find).and_return([@page])
-      do_get
-    end
-  
-    it "should assign the found pages for the view" do
-      do_get
-      assigns[:pages].should == [@page]
-    end
-  end
-
-  describe "handling GET /pages.xml" do
-
-    before(:each) do
-      @pages = mock("Array of Pages", :to_xml => "XML")
-      Page.stub!(:paginate).and_return(@pages)
-    end
-  
-    def do_get
-      @request.env["HTTP_ACCEPT"] = "application/xml"
-      get :index
-    end
-  
-    it "should be successful" do
-      do_get
-      response.should be_success
-    end
-
-    it "should find all pages" do
-      Page.should_receive(:paginate).and_return(@pages)
-      do_get
-    end
-  
-    it "should render the found pages as xml" do
-      @pages.should_receive(:to_xml).and_return("XML")
-      do_get
-      response.body.should == "XML"
-    end
-  end
-
   describe "handling GET /pages/1" do
 
     before(:each) do
