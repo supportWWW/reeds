fdd26d20f6d31bfea4eab4afb7deb8afac33223c
Joerg Diekmann <joergd@pobox.com>
2008-11-28 10:15:29 +0200

    Bits and Bobs.

--- app/mailers/stock_list_mailer.rb
+++ app/mailers/stock_list_mailer.rb
@@ -5,7 +5,7 @@ class StockListMailer < ActionMailer::Base
   def list(name, email, stocklist)
     subject    'Reeds  - Stock List'
     recipients ['joergd@pobox.com', "#{name} <#{email}>" ]
-    from        "i-am-robot-dont-respond@reeds.co.za"
+    from        "Reeds <suzanned@reeds.co.za>"
     sent_on    Time.now
     body       :name => name, :stocklist => stocklist
   end
