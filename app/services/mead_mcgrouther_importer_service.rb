818589bec4668e091b2c07cccea4ad0adf633da6
Joerg Diekmann <joergd@pobox.com>
2009-11-26 15:11:00 +0200

    Hopefully fixed MM importer

--- app/services/mead_mcgrouther_importer_service.rb
+++ app/services/mead_mcgrouther_importer_service.rb
@@ -10,19 +10,19 @@ class MeadMcgroutherImporterService
   def process( file_contents )
     result = []
     error = []
-    model_variant = nil
-    begin
-  #    file_contents.each_line do |line|
-  #      row = line.gsub( '"', '' ).split( ',' )
-  #      p "Line #{line.gsub( '"', '' )}"
-        #result << ModelVariant.find_or_create_for( row[0].strip , row[3].strip, row[2].strip, row[4].strip)
-  #   end
-      FasterCSV.parse( file_contents ) do |row|
-        model_variant = ModelVariant.find_or_create_for( row[0].strip , row[3].strip, row[2].strip, row[4].strip)
-        result << model_variant
+    line = 0
+#    file_contents.each_line do |line|
+#      row = line.gsub( '"', '' ).split( ',' )
+#      p "Line #{line.gsub( '"', '' )}"
+      #result << ModelVariant.find_or_create_for( row[0].strip , row[3].strip, row[2].strip, row[4].strip)
+#   end
+    FasterCSV.parse( file_contents ) do |row|
+      begin
+        line += 1
+        result << ModelVariant.find_or_create_for( row[0].strip , row[3].strip, row[2].strip, row[4].strip)
+      rescue
+        error << line
       end
-    rescue
-      error << model_variant
     end
     return result, error
   end
