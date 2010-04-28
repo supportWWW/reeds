1180c338208f4c4fc7ddb3828baa40b9916d0831
Francois Campbell <francois@cpt.whitewallweb.com>
2010-04-28 14:51:45 +0200

    update email address in mailer

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -1,4 +1,4 @@
 #Exception notifier
-ExceptionNotifier.exception_recipients = %w(joergd@pobox.com)
+ExceptionNotifier.exception_recipients = %w(support@whitewallweb.com)
 ExceptionNotifier.sender_address = '"Application Error" <no-reply@reeds.co.za>'
 ExceptionNotifier.email_prefix = "[Reeds error]"
