580c8c4b4bf60f4363108559df89d86670012cbe
Tich <tichaona@cpt.whitewallweb.com>
2010-11-11 15:14:07 +0200

    adding redirect enhancement

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -1,5 +1,11 @@
 class NewslettersController < ApplicationController
   def subscribe
+    if(request[:format].nil?)
+
+    elsif
+    redirect_to :action=>'subscribe'
+    end
+    
   end
 
   def unsubscribed
